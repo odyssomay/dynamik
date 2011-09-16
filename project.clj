a5b09e2a87ce363fe1f1fbe336c60e923165ad4e
Jonathan Fischer Friberg <odyssomay@gmail.com>
2011-09-16 13:04:37 +0200

    version -> 9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dynamik "8"
+(defproject dynamik "9"
   :description "A dynamic ui very similar to that of blender."
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[seesaw "1.0.10"]])
